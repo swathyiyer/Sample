b654b2f9554f922c91c9f7f923cee2177b21d40f
ssiyer <ssiyer@SUYPC097>
2018-01-24 16:26:01 +0530

    Add project files.

---
+++ CRUD/App_Start/Custommsg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace CRUD.App_Start
+{
+    public class Custommsg : IHttpActionResult
+    {
+
+        string _value;
+        HttpRequestMessage _req;
+        HttpStatusCode _code;
+        public Custommsg(string value, HttpRequestMessage req, HttpStatusCode code)
+        {
+            _value = value;
+            _req = req;
+            _code = code;
+        }
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            //throw new NotImplementedException();
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(_value),
+                RequestMessage = _req,
+                StatusCode = _code
+
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using CRUD.App_Start;
+using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUD.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        //[HttpPost]
+        //public IHttpActionResult create(int id,string name,int phoneno)
+        //{
+        //    employeeEntities ob = new employeeEntities();
+        //    employeedetail ed = new employeedetail();
+        //    ed.id = id;
+        //    ed.name = name;
+        //    ed.phoneno = phoneno;
+        //    ob.employeedetails.Add(ed);
+        //    ob.SaveChanges();
+        //    return Ok();
+        //}
+        //[HttpGet]
+        //public IHttpActionResult SearchByid(int id)
+        //{
+        //    employeeEntities ob = new employeeEntities();
+        //    employeedetail ed = new employeedetail();
+        //    var result = (from e in ob.employeedetails where (e.id == id) select e).FirstOrDefault();
+        //    return Ok(result);
+        //}
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            employeedetail ed = new employeedetail();
+            using (var context = new employeeEntities())
+            {
+                ed = context.employeedetails.Where(s => s.id == id).FirstOrDefault();
+            }
+            if (ed == null)
+            {
+                return new Custommsg("hh", Request, HttpStatusCode.NotFound);
+                //  return Request.CreateResponse(HttpStatusCode.NotFound, id);
+            }
+            return new Custommsg(ed.name, Request, HttpStatusCode.NotFound);
+        }
+
+    }
+}
